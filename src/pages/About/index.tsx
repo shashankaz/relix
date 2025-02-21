70dcb4cd2eecc1ea7ada154e1220bd26f7e2e137
Shashank <113260615+shashankaz@users.noreply.github.com>
2025-02-21 16:47:19 +0530

    feat: add about section

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,202 +1,91 @@
-import { motion, useScroll } from "motion/react";
-
 const About = () => {
-  const { scrollYProgress } = useScroll();
-
   return (
-    <div className="min-h-screen bg-black text-white font-mono">
-      <motion.div
-        style={{ scaleX: scrollYProgress }}
-        className="fixed top-0 left-0 right-0 origin-left h-1 bg-gradient-to-r from-red-500 to-pink-500"
-      ></motion.div>
-      <div className="p-20">
-        <h1 className="text-7xl text-center font-semibold mb-10">
-          Lorem ipsum dolor sit amet.
+    <div className="flex flex-col items-center justify-center py-20">
+      <div className="text-center mb-8">
+        <h6 className="text-lg font-semibold mb-2">
+          About <span>Us</span>
+        </h6>
+        <h1 className="text-4xl md:text-5xl font-semibold font-kalam">
+          Creating Beautiful Spaces
         </h1>
-        <div className="space-y-4">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            voluptates in iure eius ipsa sequi omnis sit deleniti aut dolores
-            ullam, expedita impedit? Beatae, eos provident. Ipsum voluptatum
-            vitae veritatis perferendis, laboriosam illum tempore aperiam cum,
-            recusandae quia libero nam ipsa incidunt soluta molestiae earum
-            porro quam? Expedita modi assumenda, recusandae, at quae architecto
-            esse praesentium qui quaerat excepturi quia. Necessitatibus
-            assumenda amet, harum dicta sit ipsum minus at animi sequi
-            voluptatibus quos, nam laudantium aliquid iure recusandae mollitia
-            iusto officia, facere numquam. Totam, odio, ipsum at vitae magni
-            tenetur dignissimos similique sapiente corrupti optio nemo.
-            Molestias, nesciunt quibusdam.
-          </p>
-        </div>
+        <p className="md:text-lg text-muted-foreground mt-4">
+          Learn more about our passion for design and commitment to excellence.
+        </p>
+      </div>
+      <div className="space-y-6 max-w-4xl mx-auto">
+        <p className="font-light text-center">
+          Our journey began with a simple yet powerful idea—transforming
+          ordinary spaces into extraordinary experiences. It all started with a
+          deep passion for aesthetics, functionality, and the belief that great
+          design has the power to change the way people feel, work, and live.
+          What was once a small passion project, helping friends and family
+          design their homes, quickly blossomed into a full-scale interior
+          design business, dedicated to bringing beauty and purpose to every
+          space we touch.
+        </p>
+        <p className="font-light text-center">
+          From the beginning, we knew that our approach had to be different.
+          Interior design isn't just about choosing furniture and color
+          schemes—it's about creating harmony, balance, and a space that
+          reflects the unique personality and lifestyle of its inhabitants.
+          Every project we take on is deeply personal, and we treat every home,
+          office, and commercial space as if it were our own.
+        </p>
+        <p className="font-light text-center">
+          Over the years, we have worked with a diverse range of clients—from
+          homeowners looking for cozy, elegant interiors to businesses wanting
+          to create an inspiring and productive environment. Our team is made up
+          of passionate designers, architects, and craftsmen who bring years of
+          expertise, creativity, and technical precision to every project.
+          Whether it's a modern, minimalist apartment, a luxury villa, or a
+          stylish corporate office, we approach each project with fresh ideas
+          and an unwavering commitment to excellence.
+        </p>
+        <p className="font-light text-center">
+          Our process is rooted in collaboration. We take the time to truly
+          understand our clients' vision, lifestyle, and preferences before
+          curating a design that aligns perfectly with their needs. We believe
+          that the best interiors are those that not only look stunning but also
+          enhance the way people interact with their surroundings. That’s why we
+          combine aesthetics with functionality, ensuring that every space we
+          design is both beautiful and practical.
+        </p>
+        <p className="font-light text-center">
+          Quality is at the heart of everything we do. We have built strong
+          relationships with the finest craftsmen, artisans, and suppliers to
+          source the best materials, furnishings, and decor elements. Whether
+          it's handcrafted furniture, luxurious textiles, or custom lighting
+          solutions, every detail is carefully selected to create a cohesive and
+          refined look.
+        </p>
+        <p className="font-light text-center">
+          In an industry that is constantly evolving, we stay ahead of the curve
+          by keeping up with the latest trends, technologies, and sustainable
+          practices. We are firm believers in eco-friendly and sustainable
+          design solutions, integrating environmentally responsible materials
+          and energy-efficient elements wherever possible.
+        </p>
+        <p className="font-light text-center">
+          But beyond trends and aesthetics, our ultimate goal is to create
+          spaces that tell a story. A home should feel like a sanctuary, an
+          office should inspire productivity, and a commercial space should
+          leave a lasting impression. Every space we design is a reflection of
+          the people who inhabit it, and that’s what makes our work truly
+          fulfilling.
+        </p>
+        <p className="font-light text-center">
+          Today, our brand has grown beyond what we ever imagined, but our core
+          values remain the same—passion, creativity, integrity, and a
+          relentless pursuit of excellence. Our greatest reward is seeing the
+          joy and satisfaction in our clients' eyes when they step into a space
+          that feels truly their own.
+        </p>
+        <p className="font-light text-center">
+          Whether you’re looking to revamp a single room, design your dream
+          home, or create an extraordinary commercial space, we’re here to bring
+          your vision to life. Let’s embark on this creative journey together
+          and turn your space into something remarkable.
+        </p>
       </div>
     </div>
   );
