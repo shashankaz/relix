7120000c39c0b20455eb5a1d98485bcabf3fbc35
Shashank <113260615+shashankaz@users.noreply.github.com>
2025-02-21 14:51:05 +0530

    feat: add product images and update Home page layout with links

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ArrowUpRight } from "lucide-react";
+import { Link } from "react-router";
 
 import Collections from "./_components/Collections";
 import Announcement from "./_components/Announcement";
@@ -51,19 +52,46 @@ const Home = () => {
       <div className="flex flex-col md:flex-row justify-between h-[70vh] mt-8 gap-2">
         <div className="md:w-4/5 h-full rounded-3xl flex flex-col md:flex-row gap-2">
           <div className="md:w-1/4 h-full flex flex-col gap-2">
-            <div className="w-full h-1/2 bg-black rounded-3xl"></div>
-            <div className="w-full h-1/2 bg-black rounded-3xl"></div>
+            <div className="w-full h-1/2 bg-black rounded-3xl grid place-content-center">
+              <h4 className="text-white text-4xl font-extralight text-center">
+                Style. <br /> Elegance. <br /> Space.
+              </h4>
+            </div>
+            <div className="w-full h-1/2 bg-black rounded-3xl overflow-hidden">
+              <img
+                src="/product1.jpg"
+                alt="Product 1"
+                className="h-full w-full object-cover grayscale hover:grayscale-0 transition-all duration-300"
+              />
+            </div>
           </div>
-          <div className="md:w-3/4 h-full bg-black rounded-3xl"></div>
+          <div className="md:w-3/4 h-full bg-black overflow-hidden rounded-3xl">
+            <img
+              src="/product2.jpg"
+              alt="Product 2"
+              className="h-full w-full object-cover grayscale hover:grayscale-0 transition-all duration-300"
+            />
+          </div>
         </div>
         {/* <div className="w-4/5 h-full bg-black rounded-3xl flex">
           <div className="w-1/4 h-1/2 rounded-3xl bg-black border-r-8 border-b-8 border-white"></div>
           <div className="size-10 bg-white rounded-tl-3xl"></div>
         </div> */}
         <div className="md:w-1/5 h-full flex flex-col gap-2">
-          <div className="h-1/2 bg-black rounded-3xl"></div>
+          <div className="h-1/2 bg-black overflow-hidden rounded-3xl">
+            <img
+              src="/product3.jpg"
+              alt="Product 3"
+              className="h-full w-full object-cover grayscale hover:grayscale-0 transition-all duration-300"
+            />
+          </div>
           <div className="h-1/2 bg-black rounded-3xl flex items-center justify-center">
-            <ArrowUpRight className="text-white size-[15vw]" strokeWidth={1} />
+            <Link to="/catalogue">
+              <ArrowUpRight
+                className="text-white size-[15vw] hover:scale-110 hover:rotate-6 transition-all duration-300"
+                strokeWidth={1}
+              />
+            </Link>
           </div>
         </div>
       </div>
