fe770b0146d635411771ac556552543777c2001f
Shashank <113260615+shashankaz@users.noreply.github.com>
2025-02-22 10:40:45 +0530

    feat: adjust Card component title width for better responsiveness

--- src/pages/Services/_components/Card.tsx
+++ src/pages/Services/_components/Card.tsx
@@ -9,7 +9,7 @@ const Card = ({ data, idx }: CardProps) => {
   return (
     <div className="h-40 relative overflow-hidden group">
       <div className="bg-gray-100 group-hover:bg-gray-200 transition-all duration-300 h-full w-full rounded-md p-3">
-        <h1 className="text-lg font-semibold w-1/2">{data.title}</h1>
+        <h1 className="text-lg font-semibold w-2/3 md:w-1/2">{data.title}</h1>
       </div>
       <div className="size-32 bg-black absolute -bottom-6 -right-3 rotate-[30deg] rounded-md overflow-hidden">
         <img
